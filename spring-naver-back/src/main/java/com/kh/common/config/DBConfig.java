1218df55e0d88c41f13ad3950abea649bce3bc53
kimsanghoTT <165999006+kimsanghoTT@users.noreply.github.com>
2024-07-30 17:51:43 +0900

    naverLogin

--- spring-naver-back/src/main/java/com/kh/common/config/DBConfig.java
+++ spring-naver-back/src/main/java/com/kh/common/config/DBConfig.java
@@ -20,24 +20,17 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-@PropertySource("classpath:/config.properties") // = src/main/resources
+@PropertySource("classpath:/config.properties") 
 public class DBConfig {
 
 	@Autowired
-	private ApplicationContext applicationContext; //연결되는 주소 관리자. xml과 같은 경로를 보유하고 관리
+	private ApplicationContext applicationContext; 
 	
-	//객체 생성. 히카리 사용을 선언
 	@Bean
 	@ConfigurationProperties(prefix="spring.datasource.hikari")
 	public HikariConfig hikariConfig() {
-		/*
-		HikariConfig config = new HikariConfig();
-		config.setJdbcUrl("jdbc:mysql://localhost:3306/KH_WORKBOOK");
-		config.setUsername("root");
-		config.setPassword("kh1234");
-		config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		*/
-		return new HikariConfig(); //hikari : DataBase 연결을 도와주는 라이브러리
+
+		return new HikariConfig(); 
 	}
 	
 	@Bean
@@ -51,21 +44,18 @@ public class DBConfig {
 		SqlSessionFactoryBean sfb = new SqlSessionFactoryBean();
 		sfb.setDataSource(dataSource);
 		sfb.setMapperLocations(applicationContext.getResources("classpath:/mappers/**.xml"));
-		sfb.setTypeAliasesPackage("com.kh.dto"); // 추후 본인의 dto 패키지 명으로 변경. database에 작성한 컬럼명과 dto에 작성한 변수명 대조
-		sfb.setConfigLocation(applicationContext.getResource("classpath:mybatis-config.xml")); // 추후 컬럼명을 카멜케이스 형식으로 들여옴을 설정
+		sfb.setTypeAliasesPackage("com.kh.dto"); 
+		sfb.setConfigLocation(applicationContext.getResource("classpath:mybatis-config.xml")); 
 		return sfb.getObject();
 	}
 	
-	//sql에 작성한 CRUD 작업을 관리
 	@Bean 
 	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sf) {
 		return new SqlSessionTemplate(sf);
 	}
 	
-	//커밋, 롤백을 통해 db에 완전히 저장하거나 되돌릴 수 있도록 도와줌
 	@Bean
 	public DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource) {
 		return new DataSourceTransactionManager(dataSource);
-		//insert, delete, update는 commit을 하지 않으면 완벽히 저장되지 않은 상태에서 select를 진행
 	}
 }
--- spring-naver-back/src/main/java/com/kh/controller/OAuthController.java
+++ spring-naver-back/src/main/java/com/kh/controller/OAuthController.java
@@ -1,62 +1,94 @@
 package com.kh.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import jakarta.servlet.http.HttpSession;
+
+// NaverLogin을 한 후에 로그인한 내용을 리액트에서 볼 수 있도록 NaverLoginController 파일을 수정
+// NaverLoginController.java 주소 (api url) 충돌을 막기 위해 @RequestMapping("/api")를 제거
+
 @RestController
-@RequestMapping("/api")
 public class OAuthController {
 	
-	//@Value : application.properties나 config.properties에 작성한 키 이름과 키에 담긴 값을 가져옴
-	
 	@Value("${naver.client-id}")
-	private String clientId; // = YMEPgkqT9p7r23tbgj3h
+	private String clientId; 
 	
 	@Value("${naver.client-secret}")
-	private String clientSecret; // = VaKtGlGPXp
+	private String clientSecret;
 	
 	@Value("${naver.redirect-uri}")
-	private String redirectUri; // = http://localhost:9010/naverLogin
+	private String redirectUri;
 	
 	@Value("${naver.state}")
-	private String state; // = RANDOM_STATE
+	private String state; 
 	
-	/*
-	app.get('/naverLogin', function (req, res) {
-		api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
-		res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-		res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
-	});
-	와 맞추는 getmapping
-	*/
-	@GetMapping("/naverLogin") //  api/naverLogin
+	@GetMapping("/naverLogin") 
 	public String naverLogin() {
 		String api_url = "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri + "&state=" + state;
 		return "<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>";
 	}
-	//url에 {}=변수명 표시가 없으면 @RequestParam이나 @RequestBody
-	//url에 {}=변수명 표시가 있으면 @PathVariable. {}안에 있는 변수명에 값을 집어넣음
 	@GetMapping("/callback")
-	public String callback(@RequestParam String code, @RequestParam String state) {
-		//네이버에서 로그인을 성공했을 때 받는 값
-		// 1. clientId = 어디 회사에 들어왔는가
-		// 2. clientSecret = 회사에 들어오기 위한 비밀번호
-		// 3. redirectUri = 들어오기 위한 심사를 완료했으면 나갈 위치로 전달
-		// 4. code = 네이버로부터 무사히 들어왔다는 인증코드를 받음
-		// 5. state = CSRF 공격을 방지하기 위해 사용
+	public String callback(@RequestParam String code, @RequestParam String state, HttpSession session) {
 		String api_url = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id="
 			     + clientId + "&client_secret=" + clientSecret + "&redirect_uri=" + redirectUri + "&code=" + code + "&state=" + state;
 
-		//RestTemplate = http 메서드(get,post,put,delete)를 통해 데이터를 json 형식으로 데이터를 처리
-		RestTemplate rt = new RestTemplate();
+		RestTemplate restTemplate = new RestTemplate();
 		
-		//api_url 주소로 응답받은 결과를 String(문자열)으로 가져와서 사용
-		String responseResult = rt.getForObject(api_url, String.class);
-		return responseResult;
+		// 앞의 값은 키 이름이기 때문에 String
+		// 키 이름에 담긴 값은 String이라고 확정지을 수 없으므로 Object
+		Map<String, Object> responseResult = restTemplate.getForObject(api_url, Map.class);
+		System.out.println("Token response : " + responseResult);
+		
+		// token 인증받은 값을 가져오는 데 Bearer access_token 사용
+		// 가져온 token 데이터를 문자열로 변환, 글자처럼 사용
+		String accessToken = (String) responseResult.get("access_token"); 
+		//네이버 개발자 문서에 access_token으로 로그인 허영된 값을 가져가라고 써있음
+		
+		String 유저정보가담긴Url = "https://openapi.naver.com/v1/nid/me";
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Bearer " + accessToken);
+		HttpEntity<String> entity = new HttpEntity<>("", headers);
+		//HttpEntity = 응답, 요청 모두 있음. 상세한 기능은 없음. ** ResponseEntity, RequestEntity = 각자 상세 기능 보유
+		
+		ResponseEntity<Map> userInfoRes = restTemplate.exchange(유저정보가담긴Url, HttpMethod.GET, entity, Map.class);
+		
+		Map<String, Object> userInfo = userInfoRes.getBody();
+		session.setAttribute("userInfo", userInfo); //session에 로그인 정보를 담음
+		/*
+		HttpHeaders에 인증에 대한 값을 Bearer로 가져오기
+		Bearer : 인증을 위해 서버에 제공되는 보안 토큰. 사용자가 인증을 받고 나서 API 요청을 할 때 사용
+		
+		ex) 네이버에 로그인을 하고 나면 사용자에게 로그인 됐음을 인증하는 토큰을 발급
+			추후 네이버에 로그인 된 기록을 가지고 어떠한 요청을 할 때 헤더에
+			Authorization : Bearer{} 작성하고 요청을 해야함
+			
+			* Bearer = 소유자
+			
+			Authorization : Bearer{}
+		=		권한 부여	:	권한을 가진 사람{"권한을 가지고 있는 번호"}
+		*/
+		return "redirect:";
 	}
+	
+	//가져온 네이버 정보를 리액트로 전달할 GetMapping
+	@GetMapping("/userInfo")
+	//HttpSession - > import jakarta O - 신버전 / javax X - 구버전
+	public Map<String, Object> userInfo(HttpSession session) {
+		//httpSession을 Map으로 형변환
+		Map<String, Object> userInfo = (Map<String, Object>)session.getAttribute("userInfo");
+		return userInfo;
+	}
+	
 	
 }
